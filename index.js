18ac47a936e61a07349012fd23f1c2187f7403dc
Shane <shanep42@gmail.com>
2022-12-07 14:51:17 -0600

    Front-end basics

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path')
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 5001;
 
@@ -7,6 +8,9 @@ const app = express();
 // Body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/openai', require('./routes/openaiRoutes'));
 
